8222a7aefdcae7cec7c8dd6fb0aafa7b340e6e70
gustavo <gustavogarciaci1@gmail.com>
2024-04-18 11:27:54 -0300

    config: initial setup

---
+++ configs/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  title: 'Food',
+  description:
+    'A restaurant website where you can list, add, remove and update dishes based on your own company.',
+  authors: [
+    {
+      name: 'Vinicius Alves Borba',
+      url: 'https://www.linkedin.com/in/viniciusaborba/',
+    },
+    {
+      name: 'Gustavo Antônio Cimarosti Garcia',
+      url: 'https://www.linkedin.com/in/gustavo-a-c-garcia-24b106276/',
+    },
+  ],
+}
